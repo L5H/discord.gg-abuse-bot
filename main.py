424bbd14c666f5a966cf3fe469c8c9874ab3bd15
tokenlogger <83322596+tokenlogger@users.noreply.github.com>
2021-07-12 06:34:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import discord, ctypes
+import discord, sys
 from discord.ext import commands
 from cogs import moderation, events, apicommands, ownercommands, usercommands, errorhandlers
 
-ctypes.windll.kernel32.SetConsoleTitleW('hentaihaven.dev bot')
+sys.stdout.buffer.write(b'\33]0;hentaihaven.dev bot\a')
+sys.stdout.buffer.flush()
 intents = discord.Intents.default()
 intents.members = True
 client = commands.Bot(command_prefix="h.", intents=intents)
