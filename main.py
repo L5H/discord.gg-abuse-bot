40f5d848864ed9021bda45ad7e7883bfb2df0cc1
tokenlogger <83322596+tokenlogger@users.noreply.github.com>
2021-07-13 09:53:23 -0400

    Added utilities and help command

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-import discord, os
+import discord, os, time
 from discord.ext import commands
-from cogs import moderation, events, apicommands, ownercommands, usercommands, errorhandlers
+from cogs import moderation, events, apicommands, ownercommands, usercommands, errorhandlers, utilities
 
 os.system("title hentaihaven.dev bot")
 intents = discord.Intents.default()
 intents.members = True
 client = commands.Bot(command_prefix=["h.","H."], intents=intents, case_insensitive=True)
-cogs = [errorhandlers, events, moderation, ownercommands, usercommands, apicommands]
+cogs = [errorhandlers, events, moderation, utilities, ownercommands, usercommands, apicommands]
+client.remove_command('help')
 
-for ok in range(len(cogs)):
-    cogs[ok].setup(client)
-
-client.run("no")
+a = 0
+lol = ["/","|","\̲","√"]
+while True:
+    print("Initializing.. "+lol[a%len(lol)],end='\r')
+    a += 1
+    time.sleep(.1)
+    if a == 20:
+        for ok in range(len(cogs)):
+            cogs[ok].setup(client)
+        client.run("no")
