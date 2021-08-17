09eb36863a4975305e5638dda6731a578ef28da2
tokenlogger <83322596+tokenlogger@users.noreply.github.com>
2021-08-17 16:30:46 -0400

    updated to current state

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-import discord, os, time
+import discord
 from discord.ext import commands
 from cogs import moderation, events, apicommands, ownercommands, usercommands, errorhandlers, utilities, voice
 
-os.system("title hentaihaven.dev bot")
 intents = discord.Intents.default()
 intents.members = True
-client = commands.Bot(command_prefix=["h.","H."], intents=intents, case_insensitive=True)
+client = commands.Bot(command_prefix=['g.'], intents=intents, case_insensitive=True)
 cogs = [errorhandlers, events, moderation, utilities, voice, ownercommands, usercommands, apicommands]
 client.remove_command('help')
 
-a = 0
-lol = ["/","|","\̲","√"]
-while True:
-    print("Initializing.. "+lol[a%len(lol)],end='\r')
-    a += 1
-    time.sleep(.1)
-    if a == 20:
-        for ok in range(len(cogs)):
-            cogs[ok].setup(client)
-        client.run("no")
+for ok in range(len(cogs)):
+    cogs[ok].setup(client)
+
+client.run("ODU0MDM2NDAzMTU4OTA4OTM5.YMeFYQ.-M1cBVZsnbkE79I7pHForGqNYwk")
